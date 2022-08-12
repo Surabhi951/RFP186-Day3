e2e286c1e9830fbc2fa26aa0cd1d5be52285811e
Surabhi951 <surabhibhagat95@gmail.com>
2022-08-12 13:23:03 +0530

    Added: Calculated daily wages for a month

--- src/com/bridgelabz/employeeWage/EmployeeWage.java
+++ src/com/bridgelabz/employeeWage/EmployeeWage.java
@@ -4,9 +4,7 @@ public class EmployeeWage {
     static final int WAGE_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 8;
     static final int PART_TIME__HOUR = 4;
-
     static final int IS_FULL_TIME = 2;
-
     static final int IS_PART_TIME = 1;
     static final int WORKING_DAY_PER_MONTH = 20;
 
@@ -29,7 +27,7 @@ public class EmployeeWage {
                 default:
                     System.out.println("Employee is Absent");
             }
-            System.out.println("Employee daily wage: " + empWage);
+            System.out.println("Employee daily wage: " + dayCount + " =>" + empWage);
             dayCount++;
         }
     }
