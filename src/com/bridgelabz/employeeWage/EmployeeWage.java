9bffafbf5b84d0d0c279fda0c034b1e7ad9eb908
Surabhi951 <surabhibhagat95@gmail.com>
2022-08-12 13:36:37 +0530

    Added: total working hours and days reaches for a month

--- src/com/bridgelabz/employeeWage/EmployeeWage.java
+++ src/com/bridgelabz/employeeWage/EmployeeWage.java
@@ -4,9 +4,7 @@ public class EmployeeWage {
     static final int WAGE_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 8;
     static final int PART_TIME__HOUR = 4;
-
     static final int IS_FULL_TIME = 2;
-
     static final int IS_PART_TIME = 1;
     static final int WORKING_DAY_PER_MONTH = 20;
     static final int WORKING_HOUR_PER_MONTH = 100;
